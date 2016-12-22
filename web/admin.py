d0c5ca9f987f40a9ce161af7e282f8907051ee15
jadijadi <jadijadi@gmail.com>
2016-12-22 19:24:34 +0330

    income in models and some beatification

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Income
 # Register your models here.
 
 admin.site.register(Expense)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -9,3 +9,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
