a7e149b38fc00684cd58569198a8fe7d33e063fc
jadijadi <jadijadi@gmail.com>
2016-12-25 17:17:11 +0330

    better names for expense and income

--- web/models.py
+++ web/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Passwordresetcodes(models.Model):
     code = models.CharField(max_length=32)
-    email = models.CharField(max_length = 120)
+    email = models.CharField(max_length=120)
     time = models.DateTimeField()
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50) #TODO: do not save password
