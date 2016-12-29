5c5fd5ac3c4fc9330f6664ef7dd566fc21c22623
Amir H. Sadoughi <homayoun26@gmail.com>
2016-12-29 18:47:29 -0500

    Add error checking for input in the client scripts

--- clients/commandline/bestoonincome.sh
+++ clients/commandline/bestoonincome.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-source bestoonconfig.sh
+source "$(dirname $0)"/bestoonconfig.sh
+
+print_usage()
+{
+    echo "Use this script to submit income reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
+
+# The script needs 2 arguments.
+# The first argument should be a number
+# The second argument should be a non-empty string
+if [ ! $# -eq 2 -o -z "${1##*[!0-9]*}" -o -z "${2}" ]
+then
+    print_usage
+    exit 1
+fi
+
 
 curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/income/
