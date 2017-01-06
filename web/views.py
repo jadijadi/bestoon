a3dd54ef11835cea2061ce362c7def06043b65d4
Mohammad Reza Kamalifard <mr.kamalifard@gmail.com>
2017-01-06 10:45:45 +0330

    Move utilities to utils file of application

--- web/views.py
+++ web/views.py
@@ -16,32 +16,11 @@ from postmark import PMMail
 from django.db.models import Sum, Count
 
 
+
 # Create your views here.
 
 random_str = lambda N: ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N))
 
-
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-
-def grecaptcha_verify(request):
-    data = request.POST
-    captcha_rs = data.get('g-recaptcha-response')
-    url = "https://www.google.com/recaptcha/api/siteverify"
-    params = {
-        'secret': settings.RECAPTCHA_SECRET_KEY,
-        'response': captcha_rs,
-        'remoteip': get_client_ip(request)
-    }
-    verify_rs = requests.get(url, params=params, verify=True)
-    verify_rs = verify_rs.json()
-    return verify_rs.get("success", False)
 
 
 
