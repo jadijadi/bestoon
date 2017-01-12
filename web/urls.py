c87eb6340c4b13db2a4270afbd0b31a4cdfd08bc
nuksee <shahbazi.kaveh@gmail.com>
2017-01-12 15:30:08 +0330

    Adding 'expencestat' and 'incomestat' API in order to facilitate other developers to add new features to their clients

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
     url(r'^submit/income/?$', views.submit_income, name='submit_income'),
+    url(r'^q/expencestat/?$', views.expencestat, name='expencestat'),
+    url(r'^q/incomestat/?$', views.expencestat, name='incomestat'),
     url(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
     url(r'^accounts/register/?$', views.register, name='register'),
     url(r'^accounts/whoami/?$', views.whoami, name='whoami'),
