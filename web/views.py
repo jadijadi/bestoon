e86e561003e0716e29fb3822b02a7f3cb1c53879
omdmhd <om.mo1375@gmail.com>
2017-02-22 00:21:24 +0330

    do TODO: Check if there is no `token

--- web/views.py
+++ web/views.py
@@ -135,13 +135,19 @@ def register(request):
 @csrf_exempt
 @require_POST
 def whoami(request):
-    this_token = request.POST['token']  # TODO: Check if there is no `token`
-    # Check if there is a user with this token; will retun 404 instead.
-    this_user = get_object_or_404(User, token__token=this_token)
+    if request.POST.has_key('token'):
+        this_token = request.POST['token']  # TODO: Check if there is no `token`- done-please Check it
+        # Check if there is a user with this token; will retun 404 instead.
+        this_user = get_object_or_404(User, token__token=this_token)
 
-    return JsonResponse({
-        'user': this_user.username,
-    }, encoder=JSONEncoder)  # return {'user':'USERNAME'}
+        return JsonResponse({
+            'user': this_user.username,
+        }, encoder=JSONEncoder)  # return {'user':'USERNAME'}
+
+    else:
+        return JsonResponse({
+            'message': 'لطفا token را نیز ارسال کنید .',
+        }, encoder=JSONEncoder)  #
 
 # return General Status of a user as Json (income,expense)
 
