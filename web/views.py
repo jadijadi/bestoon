37e2855233826b92197e699aafb76aea86e0814e
mohammad <mohammadho3eini73@gmail.com>
2017-08-18 13:27:42 +0430

    some code cleaning

--- web/views.py
+++ web/views.py
@@ -15,12 +15,12 @@ from .utils import grecaptcha_verify
 import random
 import string
 
-# create random string for Toekn
-random_str = lambda N: ''.join(
-    random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N))
+# create random string for token
+random_str = lambda n: ''.join(
+    random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(n))
 
 
-# login , (API) , returns : JSON = statuns (ok|error) and token
+# login , (API) , returns : JSON = status (ok|error) and token
 
 
 @csrf_exempt
@@ -34,7 +34,7 @@ def news(request):
 @require_POST
 def login(request):
     # check if POST objects has username and password
-    if request.POST.has_key('username') and request.POST.has_key('password'):
+    if 'username' in request.POST and 'password' in request.POST:
         username = request.POST['username']
         password = request.POST['password']
         this_user = get_object_or_404(User, username=username)
@@ -64,13 +64,17 @@ def register(request):
         # is this spam? check reCaptcha
         if not grecaptcha_verify(request):  # captcha was not correct
             context = {
-                'message': 'کپچای گوگل درست وارد نشده بود. شاید ربات هستید؟ کد یا کلیک یا تشخیص عکس زیر فرم را درست پر کنید. ببخشید که فرم به شکل اولیه برنگشته!'}  # TODO: forgot password
+                'message': 'کپچای گوگل درست وارد نشده بود. شاید ربات هستید؟'
+                           ' کد یا کلیک یا تشخیص عکس زیر فرم را درست پر کنید. ببخشید که فرم به شکل اولیه برنگشته!'
+            }  # TODO: forgot password
             return render(request, 'register.html', context)
 
         # duplicate email
         if User.objects.filter(email=request.POST['email']).exists():
             context = {
-                'message': 'متاسفانه این ایمیل قبلا استفاده شده است. در صورتی که این ایمیل شما است، از صفحه ورود گزینه فراموشی پسورد رو انتخاب کنین. ببخشید که فرم ذخیره نشده. درست می شه'}  # TODO: forgot password
+                'message': 'متاسفانه این ایمیل قبلا استفاده شده است. در صورتی که این ایمیل شما است،'
+                           ' از صفحه ورود گزینه فراموشی پسورد رو انتخاب کنین. ببخشید که فرم ذخیره نشده. درست می شه'
+            }  # TODO: forgot password
             # TODO: keep the form data
             return render(request, 'register.html', context)
         # if user does not exists
@@ -83,40 +87,41 @@ def register(request):
             temporarycode = Passwordresetcodes(
                 email=email, time=now, code=code, username=username, password=password)
             temporarycode.save()
-            #message = PMMail(api_key=settings.POSTMARK_API_TOKEN,
+            # message = PMMail(api_key=settings.POSTMARK_API_TOKEN,
             #                 subject="فعالسازی اکانت بستون",
             #                 sender="jadi@jadi.net",
             #                 to=email,
             #                 text_body=" برای فعال کردن اکانت بستون خود روی لینک روبرو کلیک کنید: {}?code={}".format(
             #                     request.build_absolute_uri('/accounts/register/'), code),
             #                 tag="account request")
-            #message.send()
-            message = 'ایمیلی حاوی لینک فعال سازی اکانت به شما فرستاده شده، لطفا پس از چک کردن ایمیل، روی لینک کلیک کنید.'
+            # message.send()
             message = 'قدیم ها ایمیل فعال سازی می فرستادیم ولی الان شرکتش ما رو تحریم کرده (: پس راحت و بی دردسر'
-            body = " برای فعال کردن اکانت بستون خود روی لینک روبرو کلیک کنید: <a href=\"{}?code={}\">لینک رو به رو</a> ".format(request.build_absolute_uri('/accounts/register/'), code)
+            body = " برای فعال کردن اکانت بستون خود روی لینک روبرو کلیک کنید:" \
+                   " <a href=\"{}?code={}\">لینک رو به رو</a> ".format(
+                                                                request.build_absolute_uri('/accounts/register/'), code)
             message = message + body
             context = {
-                'message': message }
+                'message': message
+            }
             return render(request, 'index.html', context)
         else:
             context = {
-                'message': 'متاسفانه این نام کاربری قبلا استفاده شده است. از نام کاربری دیگری استفاده کنید. ببخشید که فرم ذخیره نشده. درست می شه'}  # TODO: forgot password
+                'message': 'متاسفانه این نام کاربری قبلا استفاده شده است.'
+                           ' از نام کاربری دیگری استفاده کنید. ببخشید که فرم ذخیره نشده. درست می شه'
+            }  # TODO: forgot password
             # TODO: keep the form data
             return render(request, 'register.html', context)
-    elif request.GET.has_key('code'):  # user clicked on code
+    elif 'code' in request.GET:  # user clicked on code
         code = request.GET['code']
         if Passwordresetcodes.objects.filter(
                 code=code).exists():  # if code is in temporary db, read the data and create the user
-            new_temp_user = Passwordresetcodes.objects.get(code=code)
-            newuser = User.objects.create(username=new_temp_user.username, password=new_temp_user.password,
-                                          email=new_temp_user.email)
             this_token = get_random_string(length=48)
-            token = Token.objects.create(user=newuser, token=this_token)
             # delete the temporary activation code from db
             Passwordresetcodes.objects.filter(code=code).delete()
             context = {
-                'message': 'اکانت شما ساخته شد. توکن شما {} است. آن را ذخیره کنید چون دیگر نمایش داده نخواهد شد! جدی!'.format(
-                    this_token)}
+                'message': 'اکانت شما ساخته شد. توکن شما {} است.'
+                           ' آن را ذخیره کنید چون دیگر نمایش داده نخواهد شد! جدی!'.format(this_token)
+            }
             return render(request, 'index.html', context)
         else:
             context = {
@@ -133,19 +138,15 @@ def register(request):
 @csrf_exempt
 @require_POST
 def whoami(request):
-    if request.POST.has_key('token'):
+    if 'token' in request.POST:
         this_token = request.POST['token']  # TODO: Check if there is no `token`- done-please Check it
-        # Check if there is a user with this token; will retun 404 instead.
+        # Check if there is a user with this token; will return 404 instead.
         this_user = get_object_or_404(User, token__token=this_token)
 
-        return JsonResponse({
-            'user': this_user.username,
-        }, encoder=JSONEncoder)  # return {'user':'USERNAME'}
+        return JsonResponse({'user': this_user.username}, encoder=JSONEncoder)  # return {'user':'USERNAME'}
 
     else:
-        return JsonResponse({
-            'message': 'لطفا token را نیز ارسال کنید .',
-        }, encoder=JSONEncoder)  #
+        return JsonResponse({'message': 'لطفا token را نیز ارسال کنید .'}, encoder=JSONEncoder)  #
 
 
 # return General Status of a user as Json (income,expense)
@@ -215,13 +216,9 @@ def submit_income(request):
     this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
     this_token = request.POST['token'] if 'token' in request.POST else ""
     this_user = get_object_or_404(User, token__token=this_token)
+    Income.objects.create(user=this_user, amount=this_amount, text=this_text, date=this_date)
 
-    Income.objects.create(user=this_user, amount=this_amount,
-                          text=this_text, date=this_date)
-
-    return JsonResponse({
-        'status': 'ok',
-    }, encoder=JSONEncoder)
+    return JsonResponse({'status': 'ok'}, encoder=JSONEncoder)
 
 
 # submit an expanse to system (api) , input : token(POST) , output :
@@ -237,10 +234,6 @@ def submit_expense(request):
     this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
     this_token = request.POST['token'] if 'token' in request.POST else ""
     this_user = get_object_or_404(User, token__token=this_token)
+    Expense.objects.create(user=this_user, amount=this_amount, text=this_text, date=this_date)
 
-    Expense.objects.create(user=this_user, amount=this_amount,
-                           text=this_text, date=this_date)
-
-    return JsonResponse({
-        'status': 'ok',
-    }, encoder=JSONEncoder)  # return {'status':'ok'}
+    return JsonResponse({'status': 'ok'}, encoder=JSONEncoder)  # return {'status':'ok'}
