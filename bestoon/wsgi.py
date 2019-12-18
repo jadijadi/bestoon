eee1b4579602f168725618d566346e13163ea9e9
Masoud Azizi <mablue92@gmail.com>
2019-12-18 01:25:14 -0800

    All python packages upgraded to latest version(django 1 to 3)

--- bestoon/wsgi.py
+++ bestoon/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for bestoon project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bestoon.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bestoon.settings')
 
 application = get_wsgi_application()
