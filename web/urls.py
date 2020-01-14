7fc5f5cd930dca4ac01b860c78ae349c6df9745e
Muhammed Hussein karimi <m.h.karimi1383@outlook.com>
2020-01-14 15:09:56 +0330

    Remove Income and Expense added to URLs

--- web/urls.py
+++ web/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^submit/expense/?$', views.submit_expense, name='submit_expense'),
     url(r'^edit/expense/?$', views.edit_expense, name='edit_expense'),
+    url(r'^edit/rmexpense/?$', views.remove_expense, name='remove_expense'),
     url(r'^submit/income/?$', views.submit_income, name='submit_income'),
     url(r'^edit/income/?$', views.edit_income, name='edit_income'),
+    url(r'^edit/rmincome/?$', views.remove_income, name='remove_income'),
     url(r'^q/generalstat/?$', views.generalstat, name='generalstat'),
     url(r'^q/incomes/?$', views.query_incomes, name='query_incomes'),
     url(r'^q/expenses/?$', views.query_expenses, name='query_expenses'),
